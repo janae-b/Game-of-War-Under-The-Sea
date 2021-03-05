2891053f1a7c77542b9419972f231b43b4604888
janae-b <janae.boersma@comcast.net>
2021-03-05 14:16:22 -0600

    added card decks

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//variables & constants
+//look into how to randomize which cards while having them split in half
+let deck1 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+let deck2 = ["dA","dQ","dK","dJ","d10","d09","d08","d07","d06","d05","d04","d03","d02","hA","hQ","hK","hJ","h10","h09","h08","h07","h06","h05","h04","h03","h02","cA","cQ","cK","cJ","c10","c09","c08","c07","c06","c05","c04","c03","c02","sA","sQ","sK","sJ","s10","s09","s08","s07","s06","s05","s04","s03","s02"]
+let deck1drawn = []
+let deck2drawn = []
+let war1 = []
+let war2 = []
+//Cached Element References
+
+//Event Listeners
+
+//Functions
+
+
